78e0f7cbfbbe4d0d9affec063cbc6b3b2b83bb25
dineissam <dineissampro@gmail.com>
2024-03-19 14:19:24 +0100

    Question-reponse fonction

---
+++ doc.md
@@ -0,0 +1,8 @@
+Database Schema 
+----------------------
+1. User (DJango User Model)
+2. Question
+3. Answer
+4. Comment
+5. UpVotes
+6. DownVotes
---
+++ questans/main/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from .models import Answer,Question,User
+
+class AnswerForm(ModelForm):
+    class Meta:
+        model=Answer
+        fields=('detail',)
+class QuestionForm(ModelForm):
+    class Meta:
+        model=Question
+        fields=('title','detail','tags')
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model=User
+        fields=('first_name','last_name','username')
